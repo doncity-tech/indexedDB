e9693636035a835092f439fa85bb811bafa3933a
doncity-tech <princewill.opara@outlook.com>
2020-04-26 19:42:28 +0100

    still working2

--- main.js
+++ main.js
@@ -3,12 +3,36 @@ const qs = (elem) => {
   return document.querySelector(elem);
 }
 
+const cleanMyText = (text) => {
+  let temp = [];
+  text.split(' ').forEach((x) => {
+    if (x) {
+      temp.push(x);
+    }
+  })
+  if (text && temp.join(' ')) {
+    return temp.join(' ');
+  }
+}
+
 const sendToIndexedDB = () => {
   const formData = new FormData(qs('#form'));
-  let name = formData.get('name');
-  let email = formData.get('email');
-  let comment = formData.get('comment');
-  if ()
+  let name = cleanMyText(formData.get('name'));
+  let email = cleanMyText(formData.get('email'));
+  let comment = cleanMyText(formData.get('comment'));
+  if (!name && !email && !comment) { return; }
+
+  let inputData = {
+    name,
+    email,
+    comment
+  };
+  console.log(inputData);
+
+
+  qs('[name="name"]').value = "";
+  qs('[name="email"]').value = "";
+  qs('[name="comment"]').value = "";
 }
 
 qs('#submitBtn').addEventListener('click', (e) => {
